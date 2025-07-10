9635042685af03a1f3709eea17371e449b1d8e81
= <briggs.dalin09@gmail.com>
2025-07-10 13:05:02 -0600

    db--individual 2

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,40 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  // If there are no items, we can stop here or show a message
+  if (cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML =
+      "<p>Your cart is empty.</p>";
+    cartSubtotal(cartItems);
+    return;
+  }
+  // If there are items, we can render them
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Set the total price of the cart
+  cartSubtotal(cartItems);
+
+  // console.log(cartItems);
+  addRemoveButtonEventListeners();
 }
 
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
+function addRemoveButtonEventListeners() {
+  const removeButtons = document.querySelectorAll(".cart-card__remove");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", removeCartItem);
+  });
 }
 
-renderCartContents();
+function removeCartItem(event) {
+  const itemId = event.target.getAttribute("data-id");
+  const cartItems = getLocalStorage("so-cart");
+
+  const cartItemRemoved = cartItems.filter((item) => item.Id !== itemId);
+  setLocalStorage("so-cart", cartItemRemoved);
+  
+
+  renderCartContents();
+}
