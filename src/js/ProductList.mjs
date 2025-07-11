31efe8085ca7ff40aefb183bf5309d5d9dc76bb8
= <briggs.dalin09@gmail.com>
2025-07-11 17:33:20 -0600

    Moved images and json to public. Also updated cart and finished superscript

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -52,3 +52,9 @@ export default class ProductListing {
     this.renderList(filteredList);
   }
 }
+
+
+function updateCartSuperscript(count) {
+  const cartCountElement = document.querySelector(".cart-count");
+  cartCountElement.textContent = count;
+}
--- src/js/cart.js
+++ src/js/cart.js
@@ -38,3 +38,61 @@ function removeCartItem(event) {
 
   renderCartContents();
 }
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="cart-card__remove" data-id="${item.Id}">X</button>
+</li>`;
+
+  return newItem;
+}
+
+function cartSubtotal(items) {
+  const cartCard = document.querySelector(".cart-card__subtotal");
+  // console.log(items);
+  if (items.length <= 0) {
+    cartCard.classList.add("hide"); // Hide the cart subtotal
+  } else {
+    cartCard.classList.remove("hide"); // Show the cart subtotal
+
+    const subtotal = items.reduce((acc, item) => acc + item.FinalPrice, 0);
+    // console.log({ items, subtotal });
+    const cartCount = items.length;
+    // cart count
+    if (cartCount > 1) {
+      document.querySelector(".cart-count").textContent = `${cartCount} items`;
+    } else {
+      document.querySelector(".cart-count").textContent = `${cartCount} item`;
+    }
+
+    // cart subtotal
+    document.querySelector(".cart-subtotal").textContent = ` $${subtotal}`;
+  }
+}
+
+renderCartContents();
+
+function cartSuperscript(items) {
+  const cartCountElement = document.querySelector(".cart-count");
+  const cartCount = items.length;
+  if (cartCount <= 0) {
+    cartCountElement.classList.add("hide");
+  } else {
+    cartCountElement.classList.remove("hide");
+
+  if (cartCount > 0) {
+    cartCountElement.textContent = cartCount;
+  }
+ }
+}
